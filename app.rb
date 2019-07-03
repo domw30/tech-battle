06687ff7952a67b6a6b3c422525fea598a5d39b9
dominic white <domw30@gmail.com>
2019-07-03 14:50:58 +0100

    add shotgun gem, set session_secret, add test routes

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require "sinatra"
 
+set :session_secret, 'super secret'
+
 get '/' do
   "Hello World"
+end
+
+get '/secret' do
+  "Tech Battle Commence"
+end
+
+get '/battle' do
+  "time to fight"
 end
