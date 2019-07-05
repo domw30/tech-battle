b21294e7f6f7c8c533232b1130da72d41fcbfaca
dominic white <domw30@gmail.com>
2019-07-05 14:10:00 +0100

    add user story 2 feature test

---
+++ spec/features/hit_points_spec.rb
@@ -0,0 +1,9 @@
+feature 'View Player 2 HP' do
+  scenario 'Can see HP for Player 2' do
+    visit('/')
+    fill_in :player_1, with: 'User1'
+    fill_in :player_2, with: 'User2'
+    click_button 'Enter Arena'
+    expect(page).to have_content 'User2 HP: 100'
+  end
+end
