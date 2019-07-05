497dd315c55362bd105154dfea7673956a37284d
dominic white <domw30@gmail.com>
2019-07-05 14:42:51 +0100

    add and pass test to see player 1 hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -7,3 +7,13 @@ feature 'View Player 2 HP' do
     expect(page).to have_content 'User2 HP: 100'
   end
 end
+
+feature 'View Player 1 HP' do
+  scenario 'Can see HP for Player 1' do
+    visit('/')
+    fill_in :player_1, with: 'User1'
+    fill_in :player_2, with: 'User2'
+    click_button 'Enter Arena'
+    expect(page).to have_content 'User1 HP: 100'
+  end
+end
